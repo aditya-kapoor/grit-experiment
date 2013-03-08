a118501de0579b7cc795ba76bc47143dc41f1f9c
aditya-kapoor <aditya.kapoor@vinsol.com>
2013-03-08 18:19:01 +0530

    Adding .rb fil through the command line of Grit...another attempt

--- grit.rb
+++ grit.rb
@@ -5,19 +5,20 @@ repo = Grit::Repo.new(location)
 branch_name = repo.branches.first.name
 puts "Branch Name : #{branch_name}"
 
-# files = Dir.entries(location)
-# files.delete(".")
-# files.delete("..")
+files = Dir.entries(location)
+files.delete(".")
+files.delete("..")
 
-# repo.add(files)
-# repo.commit_index("Adding .rb fil through the command line of Grit...another attempt")
+repo.add(files)
+repo.commit_index("Adding .rb fil through the command line of Grit...another attempt")
 
-puts "Repository Status"
-system("git status")
+# puts "Repository Status"
+# system("git status")
 
-puts "Adding Files to Repository"
-system("git add .")
+# puts "Adding Files to Repository"
+# system("git add .")
 
 puts "Pushing to the remote origin"
-system("git push origin #{branch_name}")
-
+Dir.chdir(Dir.pwd) {
+  system("git push origin #{branch_name}")
+}
