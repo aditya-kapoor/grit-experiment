df6742aa29f83a20f424ab663fde63df77104768
aditya-kapoor <aditya.kapoor@vinsol.com>
2013-03-08 18:06:05 +0530

    Deleted file

--- grit.rb
+++ grit.rb
@@ -3,16 +3,21 @@ require 'grit'
 location = Dir.pwd
 repo = Grit::Repo.new(location)
 branch_name = repo.branches.first.name
-files = Dir.entries(location)
-files.delete(".")
-files.delete("..")
+puts "Branch Name : #{branch_name}"
 
-repo.add(files)
-repo.commit_index("Adding .rb fil through the command line of Grit...another attempt")
+# files = Dir.entries(location)
+# files.delete(".")
+# files.delete("..")
+
+# repo.add(files)
+# repo.commit_index("Adding .rb fil through the command line of Grit...another attempt")
 
 puts "Repository Status"
 system("git status")
 
+puts "Adding Files to Repository"
+system("git add .")
+
 puts "Pushing to the remote origin"
-system("git push origin #{repo.branches.first.name}")
+system("git push origin #{branch_name}")
 
